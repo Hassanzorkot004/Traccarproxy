8b2bcf0193e98655ff413dec9483ab79acb50565
Hassanzorkot004 <Hassan.ZORKOT@esprit.tn>
2025-07-08 17:38:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-TRACCAR_URL =  'https://emerging-asking-polymer-held.trycloudflare.com' 
+TRACCAR_URL =  'https://map.gpstraccar.xyz' 
 
 TRACCAR_USER = 'hassanzorkot204@gmail.com'
 TRACCAR_PASS = 'hassan@2004'
