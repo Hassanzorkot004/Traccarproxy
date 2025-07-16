4dc7017c74550da6e19f0d4383e33aafc9501888
Hassanzorkot004 <Hassan.ZORKOT@esprit.tn>
2025-07-16 14:39:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+
 from flask import Flask, request, jsonify
 import requests
 
@@ -174,5 +174,4 @@ def show_multiple():
 # 🔁 Démarrage de Flask
 # ------------------------------
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
-  app.run(debug=True, host="0.0.0.0", port=port)
+  app.run(debug=True)
