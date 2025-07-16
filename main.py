5faef4ca48ccb4f230edce154517ba31c0908be4
Hassanzorkot004 <Hassan.ZORKOT@esprit.tn>
2025-07-16 13:50:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -173,4 +173,4 @@ def show_multiple():
 # 🔁 Démarrage de Flask
 # ------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True, host="0.0.0.0", port=5000)
